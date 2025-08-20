4a8420cd2921d182fec2fd524803e837bbdab28f
sunanda143 <sunandaleitanthem888@gmail.com>
2025-08-21 03:02:00 +0530

    jio-ui-header

---
+++ Jio_Clone/jio-frontend/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="bg-black text-white p-4 rounded-lg">
+      Hello Tailwind v3 + shadcn ✅
+    </div>
+  );
+}
